e15b3ae40b3f4473b28f6d22ddfffb0d23d8cdea
hmm-shnchr <homma.shinichiro@gmail.com>
2023-06-08 04:56:08 +0900

    Sample.

--- mylib/learning_parameter.py
+++ mylib/learning_parameter.py
@@ -3,7 +3,19 @@ import importlib
 ##True  : Use CuPy
 ##False : Not use CuPy(use NumPy)
 use_cupy = False
-
+def is_cupy():
+    """
+    Returns: bool: defined this file value.
+    """
+    return use_cupy
+def xp_factory():
+    """
+    Returns: imported instance of CuPy or NumPy.
+    """
+    if is_cupy():
+        return importlib.import_module("cupy")
+    else:
+        return importlib.import_module("numpy")
 
 class LearnParam:
     """
@@ -13,53 +25,34 @@ class LearnParam:
     """
 
     def __init__(self):
-        self.THRESHOLD = 0.03
-        self.EPS = 1e-7
-        self.accuracy_test = True
-        self.accuracy_batch = True
-        self.TRAIN_MVIR_THRESHOLD = "1e+8"
-        self.TEST_MVIR_THRESHOLD = "1e+7"
-        #self.use_param_input = ["x", "y", "z", "vx", "vy", "vz", "Mvir"]
-        self.use_param_input = ["x", "vx", "Mvir"]
-        self.use_param_output = ["x"]
-        #self.use_param_output = ["x", "y", "z"]
-        self.add_bias = False
-        self.weight_decay = False
-        self.DECAY_LAMBDA = 1e-3
-        self.hidden = [100]*10
-        self.BATCH_SIZE = 10
-        self.LEARNING_RATE = "1e-3"
-        self.optimizer = "Adam"
-        self.batch_normalization = True
+        self.THRESHOLD                  = 0.03
+        self.EPS                        = 1e-7
+        self.accuracy_test              = True
+        self.accuracy_batch             = True
+        self.TRAIN_MVIR_THRESHOLD       = "1e+8"
+        self.TEST_MVIR_THRESHOLD        = "1e+7"
+        self.use_param_input            = ["x", "vx", "Mvir"]
+        self.use_param_output           = ["x"]
+        self.add_bias                   = False
+        self.weight_decay               = False
+        self.DECAY_LAMBDA               = 1e-3
+        self.hidden                     = [100]*10
+        self.BATCH_SIZE                 = 50
+        self.LEARNING_RATE              = "1e-3"
+        self.optimizer                  = "Adam"
+        self.batch_normalization        = True
         self.batch_normalization_output = False
-        self.loss_func = "RE"
-        self.activation_func = "relu"
-        self.weight_init = "he"
-        self.lastlayer_identity = True
-        self.EPOCH = 100
-        self.INPUT_SIZE = 3
-        self.OUTPUT_SIZE = 1
-        self.normalize_format = "Standardization"
-        self.learn_dataset_format = "shift"
-        self.predict_dataset_format = "equal_spaced"
-        self.extract_dataset = "After_acc"
-        self.SPLIT_EPOCH = 10
-        self.LEARN_NUM = 1
-        self.SAVE_FIG_TYPE = ".png"
-
-
-def is_cupy():
-    """
-    Returns: bool: defined this file value.
-    """
-    return use_cupy
-
-
-def xp_factory():
-    """
-    Returns: imported instance of CuPy or NumPy.
-    """
-    if is_cupy():
-        return importlib.import_module("cupy")
-    else:
-        return importlib.import_module("numpy")
+        self.loss_func                  = "RE"
+        self.activation_func            = "relu"
+        self.weight_init                = "he"
+        self.lastlayer_identity         = True
+        self.EPOCH                      = 1000
+        self.INPUT_SIZE                 = 3
+        self.OUTPUT_SIZE                = 1
+        self.normalize_format           = "Standardization"
+        self.learn_dataset_format       = "shift"
+        self.predict_dataset_format     = "equal_spaced"
+        self.extract_dataset            = "After_acc"
+        self.SPLIT_EPOCH                = 10
+        self.LEARN_NUM                  = 1
+        self.SAVE_FIG_TYPE              = ".png"
