44cf517d78d894c561834e348ef50567546e98af
hmm-shnchr <homma.shinichiro@gmail.com>
2023-06-08 04:08:23 +0900

    Add self.extract_use_param and Update comments.

---
+++ make-dataset/learning_parameter.py
@@ -0,0 +1,21 @@
+class LearnParam:
+    """
+    Describe learning parameters of a MachineLearningModel.
+    If you want to change some learning parameter,
+    rewrite those variables.
+    """
+
+    def __init__(self):
+        self.train_mvir_threshold   = "1e+8"
+        self.test_mvir_threshold    = "1e+7"
+        self.extract_user_params    = ["ScaleFactor", "Mvir", "Rvir", "x", "vx", "host_Rvir"]
+        self.use_param_input        = ["x", "vx", "Mvir"]
+        self.use_param_output       = ["x"]
+        self.input_size             = 3
+        self.output_size            = 1
+        ## "shift" or "equal_cpaced" can be specified for
+        ## self.learn_dataset_format and self.predict_dataset_format
+        self.learn_dataset_format   = "shift"
+        self.predict_dataset_format = "equal_spaced"
+        ## "All" or "Before_acc" or "After_acc" or "All_acc" or "All_not_acc" can be specified for self.extract_dataset
+        self.extract_dataset        = "After_acc"
